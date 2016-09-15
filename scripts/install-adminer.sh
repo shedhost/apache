3f1433e05f8b14ad80aeb0ccd3ae6d8196d5606b
Kevin Hamer <kevin@imarc.com>
2016-09-15 02:32:30 -0400

    extremely minimal adminer skin

--- scripts/install-adminer.sh
+++ scripts/install-adminer.sh
@@ -6,4 +6,7 @@ cd /tmp
 wget https://getcomposer.org/composer.phar
 php composer.phar create-project vrana/adminer
 mv adminer /var/shed/public/
+
+# install adminer skin
+mv /var/shed/public/adminer.css /var/shed/public/adminer/static/default.css
 rm composer.phar
